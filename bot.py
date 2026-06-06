eeec32aa6b2d130c606d1a1890e8da4fc3209d8f
Ubaydullo77 <usmanovsajfulla116@gmail.com>
2026-06-06 14:14:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 
-# Bot kaliti
-TOKEN = "AAEe_p1_xbPGcNSbvnRK_7n_oxbXSPMtM9k"
+# Yangi va to'liq tokeningiz shu yerga tushdi
+TOKEN = "8840928379:AAEe_p1_xbPGcnSbvnRK_7n_oxbXSPMtM9k"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
@@ -10,4 +10,3 @@ def start_message(message):
 
 # Botni tinimsiz ishlatish buyrug'i
 bot.infinity_polling()
-
