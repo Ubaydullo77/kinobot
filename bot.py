eeef1bad0e03c5223582807649b470fd4e096dcc
Ubaydullo77 <usmanovsajfulla116@gmail.com>
2026-06-06 14:03:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-AAEe_p1_xbPGcnSbvnRK_7n_oxbXSPMtM9k
+import telebot
+
+# Bot kaliti
+TOKEN = "AAEe_p1_xbPGcNSbvnRK_7n_oxbXSPMtM9k"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(message, "Salom! Kinobot muvaffaqiyatli ishga tushdi!")
+
+# Botni tinimsiz ishlatish buyrug'i
+bot.infinity_polling()
+
